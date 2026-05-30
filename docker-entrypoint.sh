b83afecbe560b7bfa5e5d92524092076beeab7f0
Jake Shirley <jake@xbox.com>
2026-05-29 17:30:30 -0700

    fix: Fixed docker boot error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ data_dir="${SCRAPBOOK_DATA_DIR:-/data/scrapbook}"
 if [ "$(id -u)" = "0" ]; then
   mkdir -p "$data_dir"
   chown -R node:node "$data_dir"
-  exec su node -s /bin/sh -c 'exec "$@"' -- "$@"
+  exec su node -s /bin/sh -c 'exec "$@"' sh "$@"
 fi
 
 mkdir -p "$data_dir"
